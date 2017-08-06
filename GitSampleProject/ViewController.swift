b20f4d894131a16d1c99b25d871305506e5d6833
Srinivas Mamidishetty <srinivasmamidishetty@Sri.local>
2017-08-06 22:48:40 +0530

    First commit

---
+++ GitSampleProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitSampleProject
+//
+//  Created by Srinivas Mamidishetty on 8/6/17.
+//  Copyright © 2017 Srinivas Mamidishetty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Working...")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
